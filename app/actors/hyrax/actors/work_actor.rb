299c2b279a3ae7044e77691e42c76f508c6b18cc
Tom Johnson <tom@dp.la>
2018-05-07 13:50:02 -0700

    Implement titleization on WorkActor

--- app/actors/hyrax/actors/work_actor.rb
+++ app/actors/hyrax/actors/work_actor.rb
@@ -3,6 +3,15 @@
 module Hyrax
   module Actors
     class WorkActor < Hyrax::Actors::BaseActor
+      ##
+      # This actor transforms all titles to title case.
+      #
+      # @see Hyrax::Actors::AbstractActor#create
+      def create(env)
+        env.attributes['title'].map!(&:titleize)
+
+        super
+      end
     end
   end
 end
