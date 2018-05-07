55d2f7114dd3d484c85110cb335225144aea7e37
Tom Johnson <tom@dp.la>
2018-05-07 13:38:12 -0700

    Failing test for title case in WorkActor

--- spec/actors/hyrax/actors/work_actor_spec.rb
+++ spec/actors/hyrax/actors/work_actor_spec.rb
@@ -2,5 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Hyrax::Actors::WorkActor do
   subject(:actor)  { described_class.new(next_actor) }
+  let(:ability)    { Ability.new(User.create(id: 'fake@exmaple.com')) }
+  let(:attributes) { { title: [title] } }
   let(:next_actor) { Hyrax::Actors::Terminator.new }
+  let(:env)        { Hyrax::Actors::Environment.new(work, ability, attributes) }
+  let(:title)      { 'comet in moominland' }
+  let(:work)       { Work.new }
+
+  it 'casts titles to title case' do
+    expect { actor.create(env) }
+      .to change { work.title.to_a }
+      .to a_collection_containing_exactly 'Comet In Moominland'
+  end
 end
