52b5e02c4e8f5eb1bf2a83e3eea435197ac4b056
Tom Johnson <tom@dp.la>
2018-05-07 15:24:53 -0700

    IdentiferActor tests, Spy Edition

--- spec/actors/hyrax/actors/identifier_actor_spec.rb
+++ spec/actors/hyrax/actors/identifier_actor_spec.rb
@@ -3,9 +3,19 @@ require 'rails_helper'
 RSpec.describe Hyrax::Actors::IdentifierActor do
   subject(:actor)  { described_class.new(next_actor) }
   let(:ability)    { Ability.new(User.create(id: 'fake@example.com')) }
-  let(:next_actor) { Hyrax::Actors::Terminator.new }
+  let(:next_actor) { spy_actor_class.new }
   let(:env)        { Hyrax::Actors::Environment.new(work, ability, {}) }
   let(:work)       { Work.new }
+
+  let(:spy_actor_class) do
+    Class.new do
+      attr_accessor :created
+
+      def create(_env)
+        self.created = true
+      end
+    end
+  end
 
   it 'assigns an identifier' do
     expect { actor.create(env) }
@@ -18,5 +28,11 @@ RSpec.describe Hyrax::Actors::IdentifierActor do
 
     expect(Work.new(env.attributes.merge(title: ['Comet in Moominland'])))
       .to be_valid
+  end
+
+  it 'calls create on next actor' do
+    expect { actor.create(env) }
+      .to change { next_actor.created }
+      .to true
   end
 end
