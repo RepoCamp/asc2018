e1415355086290d38434973194acc4faded81a32
Tom Johnson <tom@dp.la>
2018-05-07 14:30:59 -0700

    Naive implemetation of IdentifierActor
    I think this won't work, but it passes my tests; thinking about what to do next.

---
+++ app/actors/hyrax/actors/identifier_actor.rb
@@ -0,0 +1,11 @@
+module Hyrax
+  module Actors
+    class IdentifierActor < Hyrax::Actors::AbstractActor
+      ##
+      # @see Hyrax::Actors::AbstractActor#create
+      def create(env)
+        env.attributes['identifier'] = SecureRandom.uuid
+      end
+    end
+  end
+end
---
+++ spec/actors/hyrax/actors/identifier_actor_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Hyrax::Actors::IdentifierActor do
+  subject(:actor)  { described_class.new(next_actor) }
+  let(:ability)    { Ability.new(User.create(id: 'fake@example.com')) }
+  let(:next_actor) { Hyrax::Actors::Terminator.new }
+  let(:env)        { Hyrax::Actors::Environment.new(work, ability, {}) }
+  let(:work)       { Work.new }
+
+  it 'assigns an identifier' do
+    expect { actor.create(env) }
+      .to change { env.attributes['identifier'] }
+      .to an_instance_of(String)
+  end
+end
